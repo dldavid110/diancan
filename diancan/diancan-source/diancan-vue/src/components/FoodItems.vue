7fba9f617ce846b16fcd2e6560b1043ea42c21ee
dldavid110 <dldavid110@163.com>
2020-05-20 17:17:07 +0800

    display:flex

---
+++ diancan/diancan-source/diancan-vue/src/components/FoodItems.vue
@@ -0,0 +1,57 @@
+<template>
+	<ul>
+		<li>
+			<p class="left">123</p><p class="center"></p><p class="right"></p>
+		</li>
+		<li>
+			<p class="left">123</p><p class="center"></p><p class="right"></p>
+		</li>
+		<li>
+			<p class="left">123</p><p class="center"></p><p class="right"></p>
+		</li>
+	</ul>
+	
+	
+</template>
+
+<script>
+	export default{
+		
+		name:'FoodItems',
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	ul{
+		width:100%;
+		height:100%;
+	}
+	ul li{
+		width:100%;
+		height:120px;
+		border-bottom:solid 1px #eee;
+		display:flex;
+	}
+	 
+	ul li p.left{
+		flex:5;
+		background-color: red;
+		
+	}
+	ul li p.center{
+		background-color: green;
+		
+		flex:20;
+	}
+	ul li p.right{
+		background-color: blue;
+		flex:1;
+		
+	}
+	
+</style>
