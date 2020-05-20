3e959298c677c4d1487c37ac70f64b32ce303ba9
dldavid110 <dldavid110@163.com>
2020-05-20 17:16:08 +0800

    display:flex

--- diancan/diancan-source/diancan-vue/src/pages/Login.vue
+++ diancan/diancan-source/diancan-vue/src/pages/Login.vue
@@ -2,9 +2,11 @@
 	<div class="wrapper">
 	<Header/>
 		 <div class="main">
-		<p><input type="text" placeholder="用户名" /></p>
-		<p><input type="password" placeholder="密码" /></p>
-		<p><input type="button" value="登录" /></p>
+			 <div class="login">
+		<p><input type="text" class="txt" placeholder="用户名" /></p>
+		<p><input type="password" class="txt" placeholder="密码" /></p>
+		<p><input type="button" value="登录" class="btnlogin"/></p>
+		</div>
 		</div>
 <Footer/>
 	</div>
@@ -39,9 +41,39 @@
 		width: 100%;
 		height:calc(100% - 60px);
 		border:solid 1px green;
-		background-color: blue;
+		 
 		position:fixed;
 		top:30px;
 		left:0px;
+		overflow-y: scroll;
+	}
+	.login{
+		width:calc(100% - 100px);
+
+		height: 200px;
+		position: absolute;
+		left:50px;
+		top:calc(50% - 100px);
+		
+	}
+	.main input{
+		width:100%;
+		width:100%;
+		height:32px;
+		margin-top:5px;
+		border: solid 1px #eee;
+		border-radius: 3px;
+		
+	}
+	.main input.txt{
+		width:calc(100% - 5px);
+		height:30px;
+		margin-top:5px;
+		padding-left: 3px;
+	}
+	.btnlogin{
+		background-color:#3CB371;
+		color:#fff;
+		cursor:pointer;
 	}
 </style>
